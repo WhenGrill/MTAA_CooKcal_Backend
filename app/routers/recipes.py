0bc5693596e83b8b1b50721741f690734f377697
RichardSzarka <xszarkar@stuba.sk>
2022-03-22 18:32:22 +0100

    Implement /recipes/{recipe_id} GET endpoint
    Added /recipes/{recipe_id} GET endpoint which return a one recipe.

--- app/routers/recipes.py
+++ app/routers/recipes.py
@@ -20,3 +20,10 @@ def root(title: Optional[str] = '', db: Session = Depends(get_db)):
         answer = db.query(models.Recipe).all()
 
     return answer
+
+
+@router.get("/{recipe_id}", response_model=recipes.RecipeOut)
+def get_food(recipe_id: int, db: Session = Depends(get_db)):
+    answer = db.query(models.Recipe).filter(models.Recipe.id == recipe_id).first()
+
+    return answer
