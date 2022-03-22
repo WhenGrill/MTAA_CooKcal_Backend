2ea31cb445bdbac79823847054c9a73893383d1b
RichardSzarka <xszarkar@stuba.sk>
2022-03-22 17:35:35 +0100

    Reimplement /food/ GET endpoint
    Reimplement /food/ GET endpoint with Optional field

--- app/routers/food.py
+++ app/routers/food.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import Session
 from ..database import get_db
 from .. import models
 from ..schemas import food
-from typing import List
+from typing import List, Optional
 
 router = APIRouter(
     prefix="/food",
@@ -13,8 +13,11 @@ router = APIRouter(
 
 
 @router.get("/", response_model=List[food.FoodOut])
-def root(db: Session = Depends(get_db)):
-    answer = db.query(models.Food).all()
+def root(title: Optional[str] = '', db: Session = Depends(get_db)):
+    if title != '':
+        answer = db.query(models.Food).filter(models.Food.title == title).all()
+    else:
+        answer = db.query(models.Food).all()
 
     return answer
 
@@ -24,3 +27,4 @@ def root(food_id: int, db: Session = Depends(get_db)):
     answer = db.query(models.Food).filter(models.Food.id == food_id).first()
 
     return answer
+
