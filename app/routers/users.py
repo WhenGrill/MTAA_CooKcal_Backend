0a482d0c9a1b08b851e42f6f21737c20cba98267
RichardSzarka <xszarkar@stuba.sk>
2022-03-22 16:01:50 +0100

    Add /users GET endpoint
    Addded GET /users endpoint which returns every important user
    information.

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,6 +1,18 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from ..database import get_db
+from .. import models, schemas
+from typing import List
+
 
 router = APIRouter(
     prefix="/users",
     tags=["Users"]
 )
+
+
+@router.get("/", response_model=List[schemas.UserOut])
+def root(db: Session = Depends(get_db)):
+    answer = db.query(models.User).all()
+
+    return answer
