a7383281bf4fb5706de42428d2aaabd524b05a06
Lukáš Štrbo <xstrbol@stuba.sk>
2022-03-22 16:32:45 +0100

    Implement basic user registration

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-from ..database import get_db
-from .. import models, schemas
 from typing import List
+
+from ..database import get_db
+from .. import models, utils, schemas
+from ..schemas.users import UserOut, UserCreate
 
 
 router = APIRouter(
@@ -11,8 +13,21 @@ router = APIRouter(
 )
 
 
-@router.get("/", response_model=List[schemas.UserOut])
-def root(db: Session = Depends(get_db)):
+@router.get("/", response_model=List[UserOut])
+def get_all_users(db: Session = Depends(get_db)):
     answer = db.query(models.User).all()
 
     return answer
+
+
+@router.post("/", response_model=UserOut)
+def register_user(user_data: UserCreate, db: Session = Depends(get_db)):
+    hashed_password = utils.pwd_hash(user_data.password)
+    user_data.password = hashed_password
+
+    user_reg_data = models.User(**user_data.dict())
+    db.add(user_reg_data)
+    db.commit()
+    db.refresh(user_reg_data)
+
+    return user_reg_data
