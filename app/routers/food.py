85406ac323864778c16cc6fd86650d67b91e939b
Lukáš Štrbo <xstrbol@stuba.sk>
2022-03-23 02:16:07 +0100

    Fix /food GET endpoint
    Query for getting food via query parameter fixed to retrieve all food
    in DB via regex - '%{title}%'.

--- app/routers/food.py
+++ app/routers/food.py
@@ -1,10 +1,12 @@
 from fastapi import APIRouter
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
+from sqlalchemy import func
+from typing import List, Optional
+
 from ..database import get_db
 from .. import models
 from ..schemas import food
-from typing import List, Optional
 
 router = APIRouter(
     prefix="/food",
@@ -15,7 +17,7 @@ router = APIRouter(
 @router.get("/", response_model=List[food.FoodOut])
 def root(title: Optional[str] = '', db: Session = Depends(get_db)):
     if title != '':
-        answer = db.query(models.Food).filter(models.Food.title == title).all()
+        answer = db.query(models.Food).filter(func.lower(models.Food.title).like(f"%{title}%")).all()
     else:
         answer = db.query(models.Food).all()
 
