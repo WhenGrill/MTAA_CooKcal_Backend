649cddb5a049fc1545ddae18f2ffae7ede2ad5bb
Lukáš Štrbo <xstrbol@stuba.sk>
2022-03-24 19:45:33 +0100

    Implement function to remove None keys from dict
    This function was implemented because of error when using Optional
    fields in schemas. IF Optional was used in schema it will generate
    after.dict() function keys with None value which then caused DB
    exceptions.

--- app/utils.py
+++ app/utils.py
@@ -10,3 +10,8 @@ def pwd_hash(password: str):
 
 def verify(plain_password: str, hashed_password: str):  # Verify if password matches with hash in DB
     return pwd_context.verify(plain_password, hashed_password)
+
+
+def remove_none_from_dict(data: dict) -> dict:
+    filtered = {key: value for key, value in data.items() if value is not None}
+    return filtered
