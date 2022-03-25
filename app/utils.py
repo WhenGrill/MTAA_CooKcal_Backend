4ac24cc6fa3e65222c3da0adb29c54855ab522ed
Lukáš Štrbo <xstrbol@stuba.sk>
2022-03-26 00:47:51 +0100

    Implement image verification
    Function checks if the file being uploaded is a valid file type (being
    PNG, JPG, JPEG) - if so endpoint will raise 415 Unsupported Media Type.
    Function will also check if the file being uploaded is bigger than
    2.7MB - if so endpoint will raise 413 Entity too large.
    
    If the file passes above checks the file is checked against maximum
    dimensions - 1024x1024 - if so file will be transformed to a thumbnail.

--- app/utils.py
+++ app/utils.py
@@ -1,7 +1,16 @@
+from fastapi import HTTPException, status
 from passlib.context import CryptContext
+from typing import Union
+from PIL import Image
 
+import PIL
+import sys
+import io
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+MAX_IMAGE_DIM = 1024, 1024
+MAX_FILE_SIZE = 2831200 + sys.getsizeof(bytes())  # Maximum file size 2.7MB with bytes object size included
+ALLOWED_IMAGE_TYPES = ('PNG', 'JPEG', 'JPG')
 
 
 def pwd_hash(password: str):
@@ -15,3 +24,30 @@ def verify(plain_password: str, hashed_password: str):  # Verify if password mat
 def remove_none_from_dict(data: dict) -> dict:
     filtered = {key: value for key, value in data.items() if value is not None}
     return filtered
+
+
+def verify_image(file: bytes) -> Union[bytes, HTTPException]:  # Verify if image is suitable to upload
+    ex_unsupported = HTTPException(status_code=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+                                   detail="Unsupported file or media type")
+
+    try:
+        im = Image.open(io.BytesIO(file))
+    except PIL.UnidentifiedImageError:
+        return ex_unsupported
+    except Exception:
+        return ex_unsupported
+
+    if im.format not in ALLOWED_IMAGE_TYPES:
+        return ex_unsupported
+
+    im_size = sys.getsizeof(file)
+    if im_size > MAX_FILE_SIZE:
+        return HTTPException(status_code=status.HTTP_413_REQUEST_ENTITY_TOO_LARGE, detail="Image too large")
+
+    if im.size[0] > MAX_IMAGE_DIM[0] or im.size[0] > MAX_IMAGE_DIM[1]:
+        im.thumbnail(MAX_IMAGE_DIM, Image.ANTIALIAS)
+        img_bytes = io.BytesIO()
+        im.save(img_bytes, format=im.format)
+        return img_bytes.getvalue()
+
+    return file
