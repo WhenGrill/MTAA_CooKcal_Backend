91790931392b839426cd8951f7590cebee884c6b
Lukáš Štrbo <xstrbol@stuba.sk>
2022-03-26 17:49:44 +0100

    Fix database definition
    Added foreign keys, resetting auto increment, added constraints to
    tables and more.

--- database/tables.sql
+++ database/tables.sql
@@ -1,13 +1,27 @@
-DROP DATABASE IF EXISTS cookcal;
-
-CREATE DATABASE cookcal
-    OWNER = postgres;
-
 DROP TABLE IF EXISTS public.users;
+DROP SEQUENCE IF EXISTS users_id_seq;
+CREATE SEQUENCE users_id_seq START WITH 1;
+
+DROP TABLE IF EXISTS public.foodlist;
+DROP SEQUENCE IF EXISTS foodlist_id_seq;
+CREATE SEQUENCE foodlist_id_seq START WITH 1;
+
+DROP TABLE IF EXISTS public.weightmeasurements;
+DROP SEQUENCE IF EXISTS weightmeasurements_id_seq;
+CREATE SEQUENCE weightmeasurements_id_seq START WITH 1;
+
+DROP TABLE IF EXISTS public.recipes;
+DROP SEQUENCE IF EXISTS recipes_id_seq;
+CREATE SEQUENCE recipes_id_seq START WITH 1;
+
+DROP TABLE IF EXISTS public.food;
+DROP SEQUENCE IF EXISTS food_id_seq;
+CREATE SEQUENCE food_id_seq START WITH 1;
+
 
 CREATE TABLE public.users
 (
-    id serial NOT NULL,
+    id int NOT NULL DEFAULT nextval('users_id_seq'),
     email character varying NOT NULL,
     password character varying NOT NULL,
     profile_picture bytea,
@@ -25,14 +39,10 @@ CREATE TABLE public.users
         PRIMARY KEY (id)
 );
 
-ALTER TABLE IF EXISTS public.users
-    OWNER to postgres;
-
-DROP TABLE IF EXISTS public.weightmeasurements;
 
 CREATE TABLE public.weightmeasurements
 (
-    id serial NOT NULL,
+    id int NOT NULL DEFAULT nextval('weightmeasurements_id_seq'),
     id_user integer NOT NULL,
     weight double precision NOT NULL CHECK (weight > 0),
     measure_time timestamp with time zone NOT NULL,
@@ -45,17 +55,12 @@ CREATE TABLE public.weightmeasurements
         PRIMARY KEY (id, id_user)
 );
 
-ALTER TABLE IF EXISTS public.weightmeasurements
-    OWNER to postgres;
-
-
-DROP TABLE IF EXISTS public.recipes;
 
 CREATE TABLE IF NOT EXISTS public.recipes
 (
-    id serial NOT NULL,
+    id int NOT NULL DEFAULT nextval('recipes_id_seq'),
     id_user integer NOT NULL,
-    reciepe_picture bytea NULL,
+    recipe_picture bytea NULL,
     title character varying(80) NOT NULL,
     ingredients text NOT NULL,
     instructions text NOT NULL,
@@ -70,14 +75,10 @@ CREATE TABLE IF NOT EXISTS public.recipes
         PRIMARY KEY (id, id_user)
 );
 
-ALTER TABLE IF EXISTS public.recipes
-    OWNER to postgres;
-
-DROP TABLE IF EXISTS public.food;
 
 CREATE TABLE public.food
 (
-    id serial NOT NULL,
+    id int NOT NULL DEFAULT nextval('food_id_seq'),
     title character varying(80) NOT NULL,
     kcal_100g double precision NOT NULL CHECK(kcal_100g > 0),
 
@@ -85,14 +86,10 @@ CREATE TABLE public.food
         PRIMARY KEY (id)
 );
 
-ALTER TABLE IF EXISTS public.food
-    OWNER to postgres;
 
-
-DROP TABLE IF EXISTS public.foodlist;
 CREATE TABLE public.foodlist
 (
-    id serial NOT NULL,
+    id int NOT NULL DEFAULT nextval('foodlist_id_seq'),
     id_user integer NOT NULL,
     id_food integer NOT NULL,
     amount double precision NOT NULL CHECK (amount > 0),
@@ -100,15 +97,30 @@ CREATE TABLE public.foodlist
 
     CONSTRAINT fk_foodlist_id_user
         FOREIGN KEY (id_user)
-        REFERENCES users (id),
+        REFERENCES users (id)
+        ON DELETE CASCADE,
 
     CONSTRAINT fk_foodlist_id_food
         FOREIGN KEY (id_food)
-        REFERENCES food (id),
+        REFERENCES food (id)
+        ON DELETE SET NULL,
 
     CONSTRAINT pk_idfoodlist_id_user
         PRIMARY KEY (id, id_user)
 );
+
+
+ALTER TABLE IF EXISTS public.users
+    OWNER to postgres;
+
+ALTER TABLE IF EXISTS public.recipes
+    OWNER to postgres;
+
+ALTER TABLE IF EXISTS public.weightmeasurements
+    OWNER to postgres;
+
+ALTER TABLE IF EXISTS public.food
+    OWNER to postgres;
 
 ALTER TABLE IF EXISTS public.foodlist
     OWNER to postgres;
--- database/test_data.sql
+++ database/test_data.sql
@@ -1,19 +1,19 @@
 INSERT INTO users (email, password, first_name, last_name, gender, age, goal_weight, height,
                    state, is_nutr_adviser)
-  VALUES 
+  VALUES
         ('eget.dictum@icloud.couk', '$2a$12$nDR36dk6/RcAUA.OMTPTpuoAAKUdvjExfMxIQhdpJvbUmFjLpr3bC', 'Bernard' ,'Knowles', 1, 35, 74, 179, 1, false),
         ('lorem.ac@yahoo.com', '$2a$12$nDR36dk6/RcAUA.OMTPTpuoAAKUdvjExfMxIQhdpJvbUmFjLpr3bC', 'Jack' ,'Cooke', 1, 31, 85, 190, 0, true),
         ('aliquam.fringilla@icloud.com', '$2a$12$nDR36dk6/RcAUA.OMTPTpuoAAKUdvjExfMxIQhdpJvbUmFjLpr3bC', 'Aurora' ,'Mcneil', 0, 27, 70, 166, 0, false),
         ('augue.scelerisque@google.ca', '$2a$12$nDR36dk6/RcAUA.OMTPTpuoAAKUdvjExfMxIQhdpJvbUmFjLpr3bC', 'Burke' ,'Mcgee', 1, 42, 70, 183, 0, false),
-        ('ac.turpis@outlook.com', '$2a$12$nDR36dk6/RcAUA.OMTPTpuoAAKUdvjExfMxIQhdpJvbUmFjLpr3bC', 'Simon' ,'Carey', 1, 32, 87, 188, 0, true);
-        ('lukas@mtaa.com', '$2a$12$nDR36dk6/RcAUA.OMTPTpuoAAKUdvjExfMxIQhdpJvbUmFjLpr3bC', 'Lukas' ,'Strbo', 1, 20, 80, 188, 0, true);
+        ('ac.turpis@outlook.com', '$2a$12$nDR36dk6/RcAUA.OMTPTpuoAAKUdvjExfMxIQhdpJvbUmFjLpr3bC', 'Simon' ,'Carey', 1, 32, 87, 188, 0, true),
+        ('lukas@mtaa.com', '$2a$12$nDR36dk6/RcAUA.OMTPTpuoAAKUdvjExfMxIQhdpJvbUmFjLpr3bC', 'Lukas' ,'Strbo', 1, 20, 80, 188, 0, true),
         ('richi@mtaa.com', '$2a$12$nDR36dk6/RcAUA.OMTPTpuoAAKUdvjExfMxIQhdpJvbUmFjLpr3bC', 'Richard' ,'Szarka', 1, 21, 65, 177, 0, true);
 
 INSERT INTO weightmeasurements (id_user, weight, measure_time)
-  VALUES  
-        (0, 86, TO_TIMESTAMP('2022-03-01 9:30:20','YYYY-MM-DD HH24:MI:SS')),
-        (0, 84, TO_TIMESTAMP('2022-03-14 7:20:42','YYYY-MM-DD HH24:MI:SS')),
-        (1, 85, TO_TIMESTAMP('2022-03-15 17:48:27','YYYY-MM-DD HH24:MI:SS')),
+  VALUES
+        (1, 86, TO_TIMESTAMP('2022-03-01 9:30:20','YYYY-MM-DD HH24:MI:SS')),
+        (2, 84, TO_TIMESTAMP('2022-03-14 7:20:42','YYYY-MM-DD HH24:MI:SS')),
+        (3, 85, TO_TIMESTAMP('2022-03-15 17:48:27','YYYY-MM-DD HH24:MI:SS')),
         (2, 90, TO_TIMESTAMP('2022-01-01 16:30:20','YYYY-MM-DD HH24:MI:SS')),
         (2, 84, TO_TIMESTAMP('2022-02-01 9:36:55','YYYY-MM-DD HH24:MI:SS')),
         (2, 79, TO_TIMESTAMP('2022-03-01 6:50:01','YYYY-MM-DD HH24:MI:SS')),
@@ -21,7 +21,7 @@ INSERT INTO weightmeasurements (id_user, weight, measure_time)
         (4, 90, TO_TIMESTAMP('2022-03-11 18:08:36','YYYY-MM-DD HH24:MI:SS'));
 
 INSERT INTO food (title, kcal_100g)
-    VALUES 
+    VALUES
         ('Mlieko polotucne', 35),
         ('Hovadzie maso varene', 100),
         ('Taveny syr', 12),
@@ -38,25 +38,25 @@ INSERT INTO food (title, kcal_100g)
 
 INSERT INTO recipes (id_user, title, ingredients, instructions, kcal_100g)
     VALUES
-        (0, 'Reciepe0', 'Ingredients Here', 'Instructions here', 10),
-        (0, 'Reciepe1', 'Ingredients Here', 'Instructions here', 20),
+        (1, 'Reciepe0', 'Ingredients Here', 'Instructions here', 10),
+        (1, 'Reciepe1', 'Ingredients Here', 'Instructions here', 20),
         (1, 'Reciepe2', 'Ingredients Here', 'Instructions here', 30),
-        (1, 'Reciepe3', 'Ingredients Here', 'Instructions here', 40),
-        (1, 'Reciepe4', 'Ingredients Here', 'Instructions here', 50),
-        (1, 'Reciepe5', 'Ingredients Here', 'Instructions here', 60),
+        (2, 'Reciepe3', 'Ingredients Here', 'Instructions here', 40),
+        (2, 'Reciepe4', 'Ingredients Here', 'Instructions here', 50),
+        (2, 'Reciepe5', 'Ingredients Here', 'Instructions here', 60),
         (3, 'Reciepe6', 'Ingredients Here', 'Instructions here', 70);
 
 
 INSERT INTO foodlist (id_user, id_food, amount, time)
     VALUES
-        (0, 0, 20, now()),
-        (0, 5, 100, now()),
-        (0, 6, 5, now()),
-        (1, 8, 1, now()),
-        (1, 12, 1, now()),
-        (1, 7, 4, now()),
-        (1, 3, 100, now()),
+        (1, 0, 20, now()),
+        (1, 5, 100, now()),
+        (1, 6, 5, now()),
+        (2, 8, 1, now()),
+        (2, 12, 1, now()),
+        (3, 7, 4, now()),
+        (3, 3, 100, now()),
         (3, 0, 20, now()),
-        (3, 1, 1, now()),
-        (3, 6, 20, now()),
+        (7, 1, 1, now()),
+        (7, 6, 20, now()),
         (4, 4, 5, now());
